9fef7bf9b2d478e86f23fda0336d319f325e9f7b
Xahlicem <xahlicem@gmail.com>
2017-04-24 14:41:31 -0700

    Added RandomAnimationTile class.

---
+++ src/com/xahlicem/game/level/tile/RandomAnimatedTile.java
@@ -0,0 +1,34 @@
+package com.xahlicem.game.level.tile;
+
+import java.util.Random;
+
+import com.xahlicem.game.graphics.AnimatedSprite;
+import com.xahlicem.game.graphics.Screen;
+
+public class RandomAnimatedTile extends AnimatedTile {
+	
+	public static final Random R = new Random();
+	
+	protected final int chance;
+	protected boolean active = false;
+
+	public static final RandomAnimatedTile RGRASS = new RandomAnimatedTile(10, new int[] { 5, 30, 45, 15 }, AnimatedSprite.GRASS);
+
+	public RandomAnimatedTile(int chance, int[] delay, AnimatedSprite sprites) {
+		super(delay, sprites);
+		this.chance = chance;
+	}
+	
+	public void tick() {
+		if (active) {
+			super.tick();
+			if (delayIndex == 0) active = false;
+		}
+	}
+
+	public void draw(int x, int y, Screen screen) {
+		if (!active && R.nextInt(100000) <= chance) active = true;
+		super.draw(x, y, screen);
+	}
+
+}
