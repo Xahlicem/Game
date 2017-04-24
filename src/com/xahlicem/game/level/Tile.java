4e8330935dff862023d97397c0823bb6007eb4ef
Xahlicem <xahlicem@gmail.com>
2017-04-24 11:45:41 -0700

    Removed unused x and y variables.

--- src/com/xahlicem/game/level/Tile.java
+++ src/com/xahlicem/game/level/Tile.java
@@ -4,15 +4,14 @@ import com.xahlicem.game.graphics.Screen;
 import com.xahlicem.game.graphics.Sprite;
 
 public class Tile {
-
-	protected int x, y;
+	
 	protected Sprite sprite;
 	protected boolean solid = false;
 	
 	public static final Tile NULL = new Tile(Sprite.NULL);
 	public static final Tile DIRT = new Tile(Sprite.DIRT);
 	public static final Tile GRASS = new Tile(Sprite.GRASS);
-	public static final Tile WATER = new Tile(Sprite.WATER);
+	public static final Tile WATER = new Tile(Sprite.WATER, true);
 	
 	public Tile(Sprite sprite) {
 		this.sprite = sprite;
