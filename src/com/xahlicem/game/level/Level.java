60dc311219ea296df1328e754532fbd3de2601e0
Xahlicem <xahlicem@gmail.com>
2017-04-24 14:05:51 -0700

    Created getTile() functions, completed draw() and included WATER and GRASS in tick().

--- src/com/xahlicem/game/level/Level.java
+++ src/com/xahlicem/game/level/Level.java
@@ -5,25 +5,27 @@ import java.util.Random;
 import com.xahlicem.game.graphics.Screen;
 
 public class Level {
-	
+
 	private static Random R = new Random();
 
-	private int width, height;
+	private int width, height, wMask, hMask;
 	private int[] tiles;
-	
+
 	public Level(int width, int height) {
 		this.width = width;
 		this.height = height;
-		tiles = new int[width*height];
+		wMask = width - 1;
+		hMask = height - 1;
+		tiles = new int[width * height];
 		generateLevel();
 	}
-	
+
 	public Level(String path) {
 		loadLevel(path);
 	}
 
 	private void loadLevel(String path) {
-		
+
 	}
 
 	private void generateLevel() {
@@ -32,13 +34,32 @@ public class Level {
 				tiles[x + y * width] = R.nextInt(3);
 			}
 	}
-	
-	
+
 	public void tick() {
-		
+		AnimatedTile.WATER.tick();
+		AnimatedTile.GRASS.tick();
 	}
-	
+
 	public void draw(int xScroll, int yScroll, Screen screen) {
-		
+		screen.setOffset(xScroll, yScroll);
+		for (int y = yScroll >> 4; y <= (yScroll + screen.height) >> 4; y++)
+			for (int x = xScroll >> 4; x <= (xScroll + screen.width) >> 4; x++)
+				getTile(x & wMask, y & hMask).draw(x << 4, y << 4, screen);
+	}
+
+	public Tile getTile(int x, int y) {
+		return getTile(x + y * width);
+	}
+	public Tile getTile(int i) {
+		switch (tiles[i]) {
+			case 0:
+				return AnimatedTile.GRASS;
+			case 1:
+				return AnimatedTile.GRASS;
+			case 2:
+				return AnimatedTile.WATER;
+			default:
+				return Tile.NULL;
+		}
 	}
 }
