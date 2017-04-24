267db4eaec2c603a818477fec612c6a9251905a0
Xahlicem <xahlicem@gmail.com>
2017-04-24 14:02:10 -0700

    Created AnimatedSprites and textures.

--- src/com/xahlicem/game/graphics/AnimatedSprite.java
+++ src/com/xahlicem/game/graphics/AnimatedSprite.java
@@ -4,9 +4,11 @@ public class AnimatedSprite extends Sprite {
 
 	protected int frames;
 	protected int currentFrame = 0;
-	public int[][] sprites;
+	protected boolean increase = true;
+	protected int[][] sprites;
 
-	public static final AnimatedSprite WATER = new AnimatedSprite(3, 16, 2, 0, SpriteSheet.TILES);
+	public static final AnimatedSprite GRASS = new AnimatedSprite(4, 16, 1, 0, SpriteSheet.TILES);
+	public static final AnimatedSprite WATER = new AnimatedSprite(4, 16, 2, 0, SpriteSheet.TILES);
 
 	public AnimatedSprite(int frames, int size, int x, int y, SpriteSheet sheet) {
 		super(size, x, y, sheet);
@@ -16,8 +18,18 @@ public class AnimatedSprite extends Sprite {
 
 	public void loadAnimations(int xPos, int yPos) {
 		sprites = new int[frames][];
+		for (int i = 0; i < sprites.length; i++)
+			sprites[i] = new int[size * size];
 		for (int i = 0; i < sprites.length; i++) {
 			load(xPos, yPos + (i * size), sprites[i]);
 		}
+	}
+
+	public void nextFrame() {
+		//if (increase) currentFrame++;
+		//else currentFrame--;
+		//if (currentFrame >= frames - 1 || currentFrame <= 0) increase = !increase;
+		currentFrame = (++currentFrame) % frames;
+		pixels = sprites[currentFrame];
 	}
 }
