98e6d164db6b013407e45f7d153586f2a4f87dec
Xahlicem <xahlicem@gmail.com>
2017-04-24 14:03:06 -0700

    Added tick() function.

--- src/com/xahlicem/game/level/Tile.java
+++ src/com/xahlicem/game/level/Tile.java
@@ -4,28 +4,32 @@ import com.xahlicem.game.graphics.Screen;
 import com.xahlicem.game.graphics.Sprite;
 
 public class Tile {
-	
+
 	protected Sprite sprite;
 	protected boolean solid = false;
-	
+
 	public static final Tile NULL = new Tile(Sprite.NULL);
 	public static final Tile DIRT = new Tile(Sprite.DIRT);
 	public static final Tile GRASS = new Tile(Sprite.GRASS);
 	public static final Tile WATER = new Tile(Sprite.WATER, true);
-	
+
 	public Tile(Sprite sprite) {
 		this.sprite = sprite;
 	}
-	
+
 	public Tile(Sprite sprite, boolean solid) {
 		this.sprite = sprite;
 		this.solid = solid;
 	}
-	
+
+	public Tile tick() {
+		return this;
+	}
+
 	public void draw(int x, int y, Screen screen) {
 		screen.drawSprite(x, y, sprite);
 	}
-	
+
 	public boolean solid() {
 		return solid;
 	}
