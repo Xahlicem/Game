75d48a45c80eaaceb9e1ba6956ca60fd3a015761
Xahlicem <xahlicem@gmail.com>
2017-04-24 14:44:52 -0700

    Update chance to animate.

--- src/com/xahlicem/game/level/tile/RandomAnimatedTile.java
+++ src/com/xahlicem/game/level/tile/RandomAnimatedTile.java
@@ -6,19 +6,21 @@ import com.xahlicem.game.graphics.AnimatedSprite;
 import com.xahlicem.game.graphics.Screen;
 
 public class RandomAnimatedTile extends AnimatedTile {
-	
+
 	public static final Random R = new Random();
-	
+	public static final int CHANCE_MULTI = 100 * 60;
+
 	protected final int chance;
 	protected boolean active = false;
 
-	public static final RandomAnimatedTile R_GRASS = new RandomAnimatedTile(10, new int[] { 5, 30, 45, 15 }, AnimatedSprite.GRASS);
+	public static final RandomAnimatedTile R_GRASS = new RandomAnimatedTile(10, new int[] { 5, 30, 45, 15 },
+			AnimatedSprite.GRASS);
 
 	public RandomAnimatedTile(int chance, int[] delay, AnimatedSprite sprites) {
 		super(delay, sprites);
 		this.chance = chance;
 	}
-	
+
 	public void tick() {
 		if (active) {
 			super.tick();
@@ -27,7 +29,7 @@ public class RandomAnimatedTile extends AnimatedTile {
 	}
 
 	public void draw(int x, int y, Screen screen) {
-		if (!active && R.nextInt(100000) <= chance) active = true;
+		if (!active && R.nextInt(CHANCE_MULTI) <= chance) active = true;
 		super.draw(x, y, screen);
 	}
 
