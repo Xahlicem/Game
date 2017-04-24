706ab86941d7d160b56725242e26df14358f66a6
Xahlicem <xahlicem@gmail.com>
2017-04-24 14:03:31 -0700

    Moved pixel init back.

--- src/com/xahlicem/game/graphics/Sprite.java
+++ src/com/xahlicem/game/graphics/Sprite.java
@@ -14,6 +14,7 @@ public class Sprite {
 	public Sprite(int size, int x, int y, SpriteSheet sheet) {
 		this.size = size;
 		this.sheet = sheet;
+		pixels = new int[size*size];
 		load(x * size, y * size, pixels);
 	}
 
@@ -24,7 +25,6 @@ public class Sprite {
 	}
 
 	protected void load(int xPos, int yPos, int[] pixels) {
-		pixels = new int[size*size];
 		for (int y = 0; y < size; y++)
 			for (int x = 0; x < size; x++)
 				pixels[x + y * size] = sheet.pixels[(xPos + x) + (yPos + y) * sheet.size];
