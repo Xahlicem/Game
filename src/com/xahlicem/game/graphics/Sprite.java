d6aefe8b5f6dbbc704908cca6197d90a4f47cfc5
Xahlicem <xahlicem@gmail.com>
2017-04-24 12:01:40 -0700

    Changed load() to require and initialize pixel array.

--- src/com/xahlicem/game/graphics/Sprite.java
+++ src/com/xahlicem/game/graphics/Sprite.java
@@ -14,8 +14,7 @@ public class Sprite {
 	public Sprite(int size, int x, int y, SpriteSheet sheet) {
 		this.size = size;
 		this.sheet = sheet;
-		pixels = new int[size*size];
-		load(x * size, y * size);
+		load(x * size, y * size, pixels);
 	}
 
 	public Sprite(int size, int pixelColor) {
@@ -24,7 +23,8 @@ public class Sprite {
 		for (int i = 0; i < pixels.length; i++) pixels[i] = pixelColor;
 	}
 
-	private void load(int xPos, int yPos) {
+	protected void load(int xPos, int yPos, int[] pixels) {
+		pixels = new int[size*size];
 		for (int y = 0; y < size; y++)
 			for (int x = 0; x < size; x++)
 				pixels[x + y * size] = sheet.pixels[(xPos + x) + (yPos + y) * sheet.size];
