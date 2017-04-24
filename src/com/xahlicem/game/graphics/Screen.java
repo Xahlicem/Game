f0eb902603f0deac4d0a33dfca055a5f44b9a9e1
Xahlicem <xahlicem@gmail.com>
2017-04-24 14:04:34 -0700

    Removed unused draw() and changed clear() to use Arrays.fill().

--- src/com/xahlicem/game/graphics/Screen.java
+++ src/com/xahlicem/game/graphics/Screen.java
@@ -1,56 +1,48 @@
 package com.xahlicem.game.graphics;
 
-import java.util.Random;
+import java.util.Arrays;
 
 public class Screen {
-	private static final int MAP_SIZE = 4;
-	private static final int MAP_SIZE_MASK = MAP_SIZE - 1;
-	private int width, height;
+	public int width;
+	public int height;
+	
+	private int xOffset, yOffset;
 
 	public int[] pixels;
-	public int tiles[] = new int[MAP_SIZE * MAP_SIZE];
-
-	private Random random = new Random();
 
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		pixels = new int[width * height];
-
-		for (int i = 0; i < tiles.length; i++) {
-			tiles[i] = random.nextInt(0xFFFFFF);
-		}
+	}
+	
+	public Screen(int width, int height, int[] pixels) {
+		this.width = width;
+		this.height = height;
+		this.pixels = pixels;
 	}
 
 	public void clear() {
-		for (int i = 0; i < pixels.length; i++)
-			pixels[i] = 0;
-	}
-
-	public void draw(int camX, int camY, int[] pixels) {
-
-		for (int y = 0; y < height; y++) {
-			int yOffset = y + camY;
-			if (y < 0 || y >= height) continue;
-			for (int x = 0; x < width; x++) {
-				int xOffset = x + camX;
-				if (x < 0 || x >= width) continue;
-				//int tileIndex = (x >> 4) + (y >> 4) * 64;
-				int tileIndex = ((xOffset >> 4) & MAP_SIZE_MASK) + ((yOffset >> 4) & MAP_SIZE_MASK) * MAP_SIZE;
-				if ((tiles[tileIndex]&2) == 2)pixels[x + y * width] = Sprite.DIRT.pixels[(xOffset&15) + (yOffset&15)*16];//tiles[tileIndex];
-				else if ((tiles[tileIndex]&1) == 1)pixels[x + y * width] = Sprite.GRASS.pixels[(xOffset&15) + (yOffset&15)*16];
-				else pixels[x + y * width] = Sprite.WATER.pixels[(xOffset&15) + (yOffset&15)*16];
-			}
-
-		}
+		//for (int i = 0; i < pixels.length; i++)
+		//	pixels[i] = 0;
+		Arrays.fill(pixels, 0);
 	}
 	
 	public void drawSprite(int xPos, int yPos, Sprite sprite) {
+		xPos -= xOffset;
+		yPos -= yOffset;
 		for (int y = 0; y < sprite.size; y++) {
 			int ya = y + yPos;
 			for (int x = 0; x < sprite.size; x++) {
 				int xa = x + xPos;
+				if (xa < 0 || xa >= width || ya < 0 || ya >= height) continue;
+				pixels[xa + ya * width] = sprite.pixels[x+y*sprite.size];
 			}
 		}
+	}
+	
+	public void setOffset(int x, int y) {
+		xOffset = x;
+		yOffset = y;
 	}
 }
