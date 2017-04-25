714839a2d524615025ab92133b3e62ffb41bc4f6
Xahlicem <xahlicem@gmail.com>
2017-04-24 22:08:45 -0700

    Created TITLE level, and finished loadLevel().

--- src/com/xahlicem/game/level/Level.java
+++ src/com/xahlicem/game/level/Level.java
@@ -1,8 +1,13 @@
 package com.xahlicem.game.level;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
+
 import com.xahlicem.game.graphics.Screen;
+import com.xahlicem.game.graphics.SpriteSheet;
 import com.xahlicem.game.level.tile.Tile;
 
 public class Level {
@@ -11,6 +16,9 @@ public class Level {
 
 	private int width, height, wMask, hMask;
 	private int[] tiles;
+	public String[] bgm = new String[]{};
+
+	public static final Level TITLE = new Level("/level/TITLE.png", "BGM0");
 
 	public Level(int width, int height) {
 		this.width = width;
@@ -21,12 +29,48 @@ public class Level {
 		generateLevel();
 	}
 
+	public Level(int width, int height, String... bgm) {
+		this.width = width;
+		this.height = height;
+		wMask = width - 1;
+		hMask = height - 1;
+		this.bgm = bgm;
+		tiles = new int[width * height];
+		generateLevel();
+	}
+
 	public Level(String path) {
 		loadLevel(path);
 	}
+	
+	public Level(String path, String... bgm) {
+		loadLevel(path);
+		this.bgm = bgm;
+	}
 
 	private void loadLevel(String path) {
-
+		try {
+			BufferedImage image = ImageIO.read(SpriteSheet.class.getResource(path));
+			width = image.getWidth();
+			height = image.getHeight();
+			wMask = width - 1;
+			hMask = height - 1;
+			tiles = new int[width*height];
+			image.getRGB(0, 0, width, height, tiles, 0, width);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		for (int i = 0; i < tiles.length; i++) {
+			switch (tiles[i] & 0xFFFFFF) {
+			case 0x00FF00:
+				tiles[i] = R.nextInt(7) + 4;
+				break;
+			case 0x00FE00:
+				tiles[i] = 4;
+				break;
+			}
+		}
 	}
 
 	private void generateLevel() {
@@ -53,21 +97,25 @@ public class Level {
 	}
 
 	public Tile getTile(int i) {
-		switch (tiles[i]) {
-			case 0:
-				return Tile.GRASS_GROWN;
-			case 1:
-				return Tile.R_GRASS_GROWN;
-			case 2:
-				return Tile.GRASS_TALL;
-			case 3:
-				return Tile.R_GRASS_TALL;
-			case 4:
-				return Tile.FLOWERS;
-			case 5:
-				return Tile.R_FLOWERS;
-			default:
-				return Tile.GRASS;
-		}
+		if (tiles[i] >= Tile.tiles.size() || tiles[i] < 0) return Tile.NULL;
+		return Tile.tiles.get(tiles[i]);
+		/*switch (tiles[i]) {
+		case 0:
+			return Tile.GRASS;
+		case 1:
+			return Tile.GRASS_GROWN;
+		case 2:
+			return Tile.R_GRASS_GROWN;
+		case 3:
+			return Tile.GRASS_TALL;
+		case 4:
+			return Tile.R_GRASS_TALL;
+		case 5:
+			return Tile.FLOWERS;
+		case 6:
+			return Tile.R_FLOWERS;
+		default:
+			return Tile.NULL;
+		}*/
 	}
 }
