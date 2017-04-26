001092ef549eb6b31dc3bb32b7a8ae0fe9724cb1
Xahlicem <xahlicem@gmail.com>
2017-04-26 10:39:22 -0700

    Adjusted timing of tiles.
    - The random ones activated much too slowly
    - WATER needed a little more time.

--- src/com/xahlicem/game/level/tile/Tile.java
+++ src/com/xahlicem/game/level/tile/Tile.java
@@ -26,17 +26,17 @@ public class Tile {
 	public static int grassIndex = 0;
 
 	public static final Tile NULL = new Tile(Sprite.NULL, 0xFF66FF);
-	public static final Tile WATER = new AnimatedTile(new int[] { 240, 10, 5, 15 }, Sprite.WATER, WATER_COLOR + waterIndex++);
-	public static final Tile R_WATER = new RandomAnimatedTile(10, new int[] { 5, 10, 5, 15 }, Sprite.WATER, WATER_COLOR + waterIndex++);
+	public static final Tile WATER = new AnimatedTile(new int[] { 300, 10, 5, 15 }, Sprite.WATER, WATER_COLOR + waterIndex++);
+	public static final Tile R_WATER = new RandomAnimatedTile(100, new int[] { 5, 10, 5, 15 }, Sprite.WATER, WATER_COLOR + waterIndex++);
 	public static final Tile DIRT = new Tile(Sprite.DIRT, DIRT_COLOR + dirtIndex++);
 	public static final Tile GRASS = new Tile(Sprite.GRASS, GRASS_COLOR + grassIndex++);
 	public static final Tile SMALL_FLOWERS = new Tile(Sprite.SMALL_FLOWERS, GRASS_COLOR + grassIndex++);
 	public static final Tile GRASS_GROWN = new AnimatedTile(new int[] { 150, 30, 45, 15 }, Sprite.GRASS_GROWN, GRASS_COLOR + grassIndex++);
-	public static final Tile R_GRASS_GROWN = new RandomAnimatedTile(50, new int[] { 5, 30, 45, 15 }, Sprite.GRASS_GROWN, GRASS_COLOR + grassIndex++);
+	public static final Tile R_GRASS_GROWN = new RandomAnimatedTile(1000, new int[] { 5, 30, 45, 15 }, Sprite.GRASS_GROWN, GRASS_COLOR + grassIndex++);
 	public static final Tile GRASS_TALL = new AnimatedTile(new int[] { 150, 30, 45, 15 }, Sprite.GRASS_TALL, GRASS_COLOR + grassIndex++);
-	public static final Tile R_GRASS_TALL = new RandomAnimatedTile(50, new int[] { 5, 30, 45, 15 }, Sprite.GRASS_TALL, GRASS_COLOR + grassIndex++);
+	public static final Tile R_GRASS_TALL = new RandomAnimatedTile(2000, new int[] { 5, 30, 45, 15 }, Sprite.GRASS_TALL, GRASS_COLOR + grassIndex++);
 	public static final Tile FLOWERS = new AnimatedTile(60, Sprite.FLOWERS, GRASS_COLOR + grassIndex++);
-	public static final Tile R_FLOWERS = new RandomAnimatedTile(100, 5, Sprite.FLOWERS, GRASS_COLOR + grassIndex++);
+	public static final Tile R_FLOWERS = new RandomAnimatedTile(1500, 5, Sprite.FLOWERS, GRASS_COLOR + grassIndex++);
 
 	public Tile(Sprite sprite, int color) {
 		this.sprite = sprite;
