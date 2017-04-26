9a65f525ecb32bf79510a7e35fe29d97ff0306e0
Xahlicem <xahlicem@gmail.com>
2017-04-26 10:38:17 -0700

    Moved randomize to tick().
    - This was to save space as it really should have been in tick() anyway.

--- src/com/xahlicem/game/level/tile/RandomAnimatedTile.java
+++ src/com/xahlicem/game/level/tile/RandomAnimatedTile.java
@@ -1,7 +1,6 @@
 package com.xahlicem.game.level.tile;
 
 import com.xahlicem.game.graphics.AnimatedSprite;
-import com.xahlicem.game.graphics.Screen;
 
 public class RandomAnimatedTile extends AnimatedTile {
 	public static final int CHANCE_MULTI = 100 * 60 * 10;
@@ -20,15 +19,11 @@ public class RandomAnimatedTile extends AnimatedTile {
 	}
 
 	public void tick() {
+		if (!active && R.nextInt(CHANCE_MULTI) <= chance) active = true;
 		if (active) {
 			super.tick();
 			if (delayIndex == 0) active = false;
 		}
-	}
-
-	public void draw(int x, int y, Screen screen, int darkness) {
-		if (!active && R.nextInt(CHANCE_MULTI) <= chance) active = true;
-		super.draw(x, y, screen, darkness);
 	}
 
 }
